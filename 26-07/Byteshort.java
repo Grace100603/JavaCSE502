f2e1df031bac938d8c075426f1f5c79c479a67df
GraceEvangeline <88892193+Grace100603@users.noreply.github.com>
2023-07-26 15:48:19 +0530

    Add files via upload

---
+++ 26-07/Byteshort.java
@@ -0,0 +1,17 @@
+package Array;
+
+public class Byteshort {
+	static void m(int i)
+	{
+		System.out.println("int");
+	}
+	static void m(Integer i)
+	{
+		System.out.println("Integer");
+	}
+	public static void main(String args[])
+	{
+		short s=30;
+		m(s);
+	}
+}
