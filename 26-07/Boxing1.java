055a61f94b010cff5ed240f354245c8019944d62
GraceEvangeline <88892193+Grace100603@users.noreply.github.com>
2023-07-26 15:07:52 +0530

    Add files via upload

---
+++ 26-07/Boxing1.java
@@ -0,0 +1,11 @@
+package Array;
+
+public class Boxing1 {
+	public static void main(String args[])
+	{
+		int a=50;
+		Integer a2=new Integer(a);//Boxing
+		Integer a3=5;//Boxing
+		System.out.println(a2+" "+a3);
+	}
+}
